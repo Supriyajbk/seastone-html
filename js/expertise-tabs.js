207957582b8ff67645686e4ce50493a16363b0a6
Supriyajbk <supriya@jbkinfotech.com>
2024-11-18 12:19:10 +0530

    hhh

--- js/expertise-tabs.js
+++ js/expertise-tabs.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
     let $jq = jQuery.noConflict();
 
     let $links = $jq("ul.expertise-links > li > a");
@@ -8,32 +8,40 @@
     let $sliderfor = $jq(".expertise-rows");
 
     let $desktop = function() {
-        return $jq(window).width() >= 768; // if you want desktop design for ipad change 1024 to 768
-    }
+        return $jq(window).width() >= 768;
+    };
 
     function initializeSlick() {
-        if (!$desktop()) {
+        if (!$desktop() && !$slidernav.hasClass('slick-initialized')) {
             $slidernav.slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                Infinity: false,
                 arrows: true,
+                prevArrow: '<div class="slick-arrow slick-prev flex flex-center radius-50" aria-label="Previous Arrow" role="button"><span><i class="fa-regular fa-chevron-left"></i></span></div>',
+                nextArrow: '<div class="slick-arrow slick-next flex flex-center radius-50" aria-label="Next Arrow" role="button"><span><i class="fa-regular fa-chevron-right"></i></span></div>',
                 dots: false,
                 asNavFor: $sliderfor,
             });
+
             $sliderfor.slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                Infinity: false,
                 asNavFor: $slidernav,
                 fade: true,
                 dots: false,
                 arrows: false,
-                focusOnSelect: true
+                focusOnSelect: true,
+                adaptiveHeight: true,
             });
 
-            $links.click(function(e) {
+            $links.off("click").on("click", function(e) {
                 e.preventDefault();
-                let slideno = $jq(this).data('name');
-                $sliderfor.slick('slickGoTo', slideno - 1);
+                let slideno = parseInt($jq(this).data('name'), 10);
+                if (!isNaN(slideno) && $sliderfor.hasClass('slick-initialized')) {
+                    $sliderfor.slick('slickGoTo', slideno - 1);
+                }
             });
         }
     }
@@ -51,7 +59,8 @@
 
     function handleResize() {
         if ($desktop()) {
-            $links.on("click", function(e) {
+            destroySlick();
+            $links.off("click").on("click", function(e) {
                 e.preventDefault();
                 $jq(this).parent().siblings('li').find("a").removeClass("active");
                 $jq(this).addClass("active");
@@ -59,7 +68,6 @@
                 $jq(".expertise-row").fadeOut(100);
                 $jq(".expertise-row[data-value=" + attr + "]").fadeIn(800);
             });
-            destroySlick();
         } else {
             initializeSlick();
         }
@@ -67,6 +75,5 @@
 
     handleResize();
 
-    $jq(window).load(handleResize);
-    $jq(window).resize(handleResize);
+    $jq(window).on("load resize", handleResize);
 })();
